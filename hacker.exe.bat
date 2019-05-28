9be86a7a1862218983e69863b48981bfc1285ede
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-05-28 16:16:57 -0400

    Create hacker.exe.bat

---
+++ hacker.exe.bat
@@ -0,0 +1,53 @@
+@echo off
+title MainFrameHackers.exe.bat
+CLS
+ECHO WARNING THY MORTAL -- YOU ARE ABOUT TO ENTER THE MAINFRAME!
+ECHO IF YOU ARE NOT READY FOR THIS, CLOSE THIS PROGRAM NOW!!!!!!!!!!
+ECHO BUT IF YOU ARE READY TO START L337 HACKING, HIT ENTER!! (69 SOFTWARE)
+PAUSE
+TITLE MAIN FRAME IN PROGRESS!!
+CLS
+color 20
+C:
+cd C:\
+echo Please wait the mainframe detects the mystical "tree.com"
+title Searching for "tree.com" to begin hacking!
+timeout /t 10 >nul
+echo Prepared to start searching...
+timeout /t 2 >nul
+DIR /S
+ECHO FILE DETECTED!!!!
+TIMEOUT /T 3 >NUL
+CLS
+DIR /S TREE.COM
+TIMEOUT /T 3 >NUL
+CLS
+ECHO WARNING! YOU HAVE PREPARED TO ENTER THE MAIN FRAME FINALLY!
+ECHO IF YOU NOT AN EPIC L33T HACKER, PLEASE LEAVE NOW!!!
+ECHO (ALARM) LAST WARNING!!!! (ALARM)
+PAUSE
+CLS
+TITLE EPIC HACKER CONSOLE V4.20
+ECHO PLEASE SELECT YOUR OPTION
+ECHO 1. ENTER THE MAINFRAME
+ECHO 2. HACK THE MAINFRAME
+ECHO 3. DESTROY THE MAINFRAME
+ECHO 4. CREATE A MALWARE VIRUS TROJAN
+echo --PLEASE SELECT YOUR OPTION--
+pause>nul || rem super b1g input readR
+cls
+title MAINFRAME HACKER .COM
+echo GOOD OPTION! PRESS ANY KEY TO COMMENCE!
+pause>nul
+cls
+:loop
+title Please wait while the Mainframe is being hacked.
+echo MAINFRAME HACKERS
+cd..
+cd..
+cd..
+cd C:\
+cd \
+tree
+netstat
+goto :loop
